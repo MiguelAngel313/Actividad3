2550c32fb2132d012b40c612978a4e1db6ab65e4
MiguelAngel313 <rmiguelangel222@gmail.com>
2025-01-24 12:19:24 +0100

    Creacion de clase libros y gestion de libros

---
+++ src/GestionLibros.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GestionLibros {
+    
+
+    public Libro agregarLibrosNuevos(){
+        Scanner entrada=new Scanner(System.in);
+        Libro libro=new Libro();
+        System.out.println("Introduce un Id:");
+        libro.setIdLibro(entrada.nextInt());
+        System.out.println("Introduce un Titulo:");
+        
+        
+    }
+}
--- src/Libro.java
+++ src/Libro.java
@@ -1,4 +1,46 @@
 public class Libro {
 
+    private int idLibro;
+    private String titulo;
+    private String autor;
+    private int numeroPaginas;
+    private String categoria;
+    public int getIdLibro() {
+        return idLibro;
+    }
+    public void setIdLibro(int idLibro) {
+        this.idLibro = idLibro;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    @Override
+    public String toString() {
+        return "Libro [idLibro=" + idLibro + ", titulo=" + titulo + ", autor=" + autor + ", numeroPaginas="
+                + numeroPaginas + ", categoria=" + categoria + "]";
+    }
+
+   
     
 }
