a4e77e15bc92ac88b50a94f3fabef3d84849a3f8
MiguelAngel313 <rmiguelangel222@gmail.com>
2025-01-28 20:05:12 +0100

    Eliminacion de clases antiguas

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,65 +1,41 @@
 public class Usuario {
-        private String nombre;
-        private String email;
-        private String contrasenia;
-        private boolean isAdmi;
-
-
-public Usuario(String nombre, String email, String contrasenia, boolean isAdmi ){
-    this.nombre = nombre;
-    this.email = email;
-    this.contrasenia = contrasenia;
-    this.isAdmi= isAdmi;
-    
-}
-
-    public String getNombre() {
-        return this.nombre;
+    private int idUsuario;
+    private boolean admin;
+    private String nombre;
+    private int edad;
+    public Usuario(int idUsuario, boolean admin, String nombre, int edad) {
+        this.idUsuario = idUsuario;
+        this.admin = admin;
+        this.nombre = nombre;
+        this.edad = edad;
     }
-
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    public boolean isAdmin() {
+        return admin;
+    }
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+    public String getNombre() {
+        return nombre;
+    }
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
-    public String getEmail() {
-        return this.email;
+    public int getEdad() {
+        return edad;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public void setEdad(int edad) {
+        this.edad = edad;
     }
-
-    public String getContrasenia() {
-        return this.contrasenia;
-    }
-
-    public void setContrasenia(String contrasenia) {
-        this.contrasenia = contrasenia;
-    }
-
-    public boolean isEsAdmi() {
-        return this.isAdmi;
-    }
-
-    public boolean getEsAdmi() {
-        return this.isAdmi;
-    }
-
-    public void setEsAdm(boolean isEsAdmi) {
-        this.isAdmi = isEsAdmi;
-    }
-
     @Override
     public String toString() {
-        return "{" +
-            " nombre='" + nombre + "'" +
-            ", email='" + email + "'" +
-            ", contrasenia='" + contrasenia + "'" +
-            ", isAdm='" + isAdmi + "'" +
-            "}";
+        return "Usuario [idUsuario=" + idUsuario + ", admin=" + admin + ", nombre=" + nombre + ", edad=" + edad + "]";
     }
 
-
-
 }
-
