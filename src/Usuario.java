52d3e005200bc92bb97d3b29f5e600034fdebb8c
MiguelAngel313 <rmiguelangel222@gmail.com>
2025-01-28 20:27:36 +0100

    Revert "Merge branch 'feature/gestionUsuarios' of https://github.com/MiguelAngel313/Actividad3 into miguel"
    This reverts commit a325b93b7970a4fdb2234723ca29e6ee9d2f58c7, reversing
    changes made to 98658343446a97caddf4bac11881521b5b98933b.

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,5 +1,4 @@
 public class Usuario {
-<<<<<<< HEAD
     private int idUsuario;
     private boolean admin;
     private String nombre;
@@ -24,24 +23,6 @@ public class Usuario {
     }
     public String getNombre() {
         return nombre;
-=======
-        private String nombre;
-        private String email;
-        private String contrasenia;
-        private boolean isAdmin;
-
-
-public Usuario(String nombre, String email, String contrasenia, boolean isAdmin ){
-    this.nombre = nombre;
-    this.email = email;
-    this.contrasenia = contrasenia;
-    this.isAdmin= isAdmin;
-    
-}
-
-    public String getNombre() {
-        return this.nombre;
->>>>>>> e3553b99db97b0ff7a7eac18514060decbe55b3d
     }
     public void setNombre(String nombre) {
         this.nombre = nombre;
@@ -52,41 +33,9 @@ public Usuario(String nombre, String email, String contrasenia, boolean isAdmin 
     public void setEdad(int edad) {
         this.edad = edad;
     }
-<<<<<<< HEAD
     @Override
     public String toString() {
         return "Usuario [idUsuario=" + idUsuario + ", admin=" + admin + ", nombre=" + nombre + ", edad=" + edad + "]";
-=======
-
-    public String getContrasenia() {
-        return this.contrasenia;
-    }
-
-    public void setContrasenia(String contrasenia) {
-        this.contrasenia = contrasenia;
-    }
-
-    public boolean isAdmin() {
-        return this.isAdmin;
-    }
-
-    public boolean getAdmin() {
-        return this.isAdmin;
-    }
-
-    public void setEsAdm(boolean isAdmin) {
-        this.isAdmin = isAdmin;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-            " nombre='" + nombre + "'" +
-            ", email='" + email + "'" +
-            ", contrasenia='" + contrasenia + "'" +
-            ", isAdmin='" + isAdmin() + "'" +
-            "}";
->>>>>>> e3553b99db97b0ff7a7eac18514060decbe55b3d
     }
 
 }
