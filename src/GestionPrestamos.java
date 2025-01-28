e3627525c0f9480915944535a96c57a98e58b601
MiguelAngel313 <rmiguelangel222@gmail.com>
2025-01-28 23:19:09 +0100

    Creacion de la clase Gestion prestamos y finallizacion de la penultima funcion.

---
+++ src/GestionPrestamos.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class GestionPrestamos {
+    
+public void realizarPrestamosLibros(ArrayList<Libro> libros, int id){
+
+    int vecesPrestado;
+    for (Libro libro : libros) {
+        if (libro.getIdLibro()==id && libro.isPrestado()==false) {
+            vecesPrestado=libro.getNumVecesPrestado();
+            libro.setPrestado(true);
+            libro.setNumVecesPrestado(vecesPrestado++);
+        }
+    }
+}
+
+public void devolverLibroPrestado(ArrayList<Libro> libros, int id){
+
+    for (Libro libro : libros) {
+        if (libro.getIdLibro()==id && libro.isPrestado()==true) {
+            libro.setPrestado(false);
+        }
+    }
+}
+
+}
