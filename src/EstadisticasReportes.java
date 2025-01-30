15ce9cffbd635101d12ba5aef8faf3010447d560
MiguelAngel313 <rmiguelangel222@gmail.com>
2025-01-30 18:43:15 +0100

    Implementacion de la penultima funcion de la clase EStadisticasReportes

---
+++ src/EstadisticasReportes.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class EstadisticasReportes {
+
+    public void mostrarPrestamosTotalesyActivos(ArrayList<Libro> libros, ArrayList<Usuario> users){
+        int prestamosTotales=0;
+        int prestamosActivos=0;
+
+        for (Libro libro : libros) {
+            prestamosTotales+=libro.getNumVecesPrestado();
+        }
+
+        for (Usuario usuario : users) {
+            prestamosActivos+=usuario.getNumeroDePrestamosActivos();
+        }
+
+        System.out.println("->Prestamos Totales: \n"+prestamosTotales);
+        System.out.println("->Prestamos Activos: \n"+prestamosActivos);
+    }
+
+    public void mostrarLibrosMasPrestados(ArrayList<Libro> libros){
+
+        int mayor=0;
+        Libro libro1=null;
+        Libro libro2=null;
+
+        for (Libro libro : libros) {
+            if (libro.getNumVecesPrestado()>mayor) {
+                libro1=libro;
+                mayor=libro.getNumVecesPrestado();
+            }
+        }
+
+        mayor=0;
+
+        for (Libro libro : libros) {
+            if (libro!=libro1) {
+                if (libro.getNumVecesPrestado()>mayor) {
+                    libro2=libro;
+                    mayor=libro.getNumVecesPrestado();
+                }
+            }
+        }
+        System.out.println("Libros mas prestados: \n"+libro1.getTitulo()+"\n"+libro2.getTitulo());
+
+    }
+
+    //Implementar ultima funcion del usuario con mas prestamos activos
+    
+}
